b171f33ce73aefe04805c7841e58b2363bb3717a
Bruno <broonohsilva@gmail.com>
2025-12-26 01:20:51 -0300

    correção url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
-
+//const api = axios.create({
+// baseURL: "http://127.0.0.1:8000",
+//});
+const api = axios.create({ baseURL: "https://controla-custos.onrender.com" });
 // Isso garante que toda requisição envie o token se ele existir no localStorage
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
